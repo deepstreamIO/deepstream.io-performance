4c3c09cd00c5e0095d7c7fe614bcd5beb94b768a
Yasser Fadl <yasser.fadl@gmail.com>
2015-07-20 18:40:06 +0200

    Pointing to correct ds

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 module.exports = {
 	server: {
 		deepstreams: [ 7000, 7001, 7002 ],
-		host: 'ec2-52-28-172-103.eu-central-1.compute.amazonaws.com',
+		host: 'ec2-52-28-146-161.eu-central-1.compute.amazonaws.com',
 		spawningSpeed: 1000,
 		totalTestTime: 10000,
 		logLevel: 3
